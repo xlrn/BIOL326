4301a7724f986945d927a1abd6df4fe6d3a4530f
Nelson Yim <ncl.yim@gmail.com>
2018-03-11 21:53:05 -0700

    add lab 6, updated gitignore

---
+++ Lab6.R
@@ -0,0 +1,18 @@
+setwd("~/Documents/Notes/BIOL326/Lab6")
+data <- read.csv("Lab data_v2.csv", stringsAsFactors = FALSE, strip.white = TRUE, na.strings = c("NA", ""))
+data2 <- read.csv("Lab data_v2sorted.csv", stringsAsFactors = FALSE, strip.white = TRUE, na.strings = c("NA", ""))
+dataNoCold <- data2[data2$Condition.Code == 'VT' || data2$Condition.Code == 'CN', ]
+attached <- as.factor(data$Attached)
+cold <- data$Cold
+variability <- data$Var
+
+attachedTime <- as.factor(data2$Time.to.attach..s...NR...not.recorded)
+variability2 <- data2$Condition.Code
+
+z <- glm(attached ~ variability * cold, family = binomial(link="logit"), data = data)
+z
+summary(z)
+z_anova <- anova(z, test = "Chisq")
+z_anova
+
+test2 <- lm()
